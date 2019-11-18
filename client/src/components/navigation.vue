43c31e9b6d902b8049c913ca5d00f394d7469abc
Bogdan Vasile <evasile@hogash.com>
2019-11-18 11:31:33 +0200

    Stitch client

--- client/src/components/navigation.vue
+++ client/src/components/navigation.vue
@@ -8,32 +8,26 @@
 			<b-collapse id="nav-collapse" is-nav>
 				<b-navbar-nav>
 					<b-nav-item href="#">
-						<router-link to="/">Home</router-link>
+						<router-link to="/" tag="span">Home</router-link>
 					</b-nav-item>
 					<b-nav-item href="#">
-						<router-link to="/posts">Posts</router-link>
+						<router-link to="/posts" tag="span">Posts</router-link>
 					</b-nav-item>
 				</b-navbar-nav>
 
 				<!-- Right aligned nav items -->
 				<b-navbar-nav class="ml-auto">
-					<b-nav-form>
+					<!-- <b-nav-form>
 						<b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
 						<b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-					</b-nav-form>
-
-					<b-nav-item-dropdown text="Lang" right>
-						<b-dropdown-item href="#">EN</b-dropdown-item>
-						<b-dropdown-item href="#">ES</b-dropdown-item>
-						<b-dropdown-item href="#">RU</b-dropdown-item>
-						<b-dropdown-item href="#">FA</b-dropdown-item>
-					</b-nav-item-dropdown>
+					</b-nav-form>-->
 
 					<b-nav-item-dropdown right>
 						<!-- Using 'button-content' slot -->
 						<template v-slot:button-content>
-							<em>User</em>
+							<em>Login</em>
 						</template>
+						<b-dropdown-item @click="googleLogin" href="#">Google Login</b-dropdown-item>
 						<b-dropdown-item href="#">Profile</b-dropdown-item>
 						<b-dropdown-item href="#">Sign Out</b-dropdown-item>
 					</b-nav-item-dropdown>
@@ -47,6 +41,7 @@
 <script>
 import PostService from "../postService.js";
 import { BNavbar } from "bootstrap-vue";
+import { addAuthenticationListener } from "../StitchAuth";
 
 export default {
 	name: "navigation",
@@ -58,10 +53,15 @@ export default {
 		return {
 			posts: [],
 			error: "",
-			text: ""
+			text: "",
+			test: "",
+			credentials: ""
 		};
 	},
 	methods: {
+		googleLogin(){
+
+		},
 		async createPost() {
 			await PostService.insertPost(this.text);
 			const postsData = await PostService.getPosts();
@@ -75,6 +75,12 @@ export default {
 		}
 	},
 	async created() {
+		let appId = "node-admin";
+		const client = Stitch.initializeDefaultAppClient(appId);
+		this.test = client.auth.isLoggedIn;
+		// const credentials = new Stitch.GoogleRedirectCredential();
+		// this.credentials = credentials;
+		// client.auth.loginWithRedirect(credentials);
 		try {
 			const postsData = await PostService.getPosts();
 			this.posts = postsData.posts;
